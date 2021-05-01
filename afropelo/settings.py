1cfb5286b2361a60256d6eecc26fbfe13def6496
tosinfarai <tosinfarai@gmail.com>
2021-05-01 22:00:15 +0100

    change shop to products in navbar

--- afropelo/settings.py
+++ afropelo/settings.py
@@ -144,7 +144,6 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
